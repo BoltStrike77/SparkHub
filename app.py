3f3395d545564718f37303daa19a99f7a71e76f7
AkhilKrishna16 <akhil.krishnamurthy2006@gmail.com>
2023-06-13 15:03:53 -0500

    Updated for status AND Top500

--- app.py
+++ app.py
@@ -4,17 +4,24 @@ import pandas as pd
 
 app = Flask(__name__)
 
-def load_model():
-    with open('model.pkl', 'rb') as file:
+def load_status_predictor():
+    with open('xgb_status.pkl', 'rb') as file:
         model = pickle.load(file)
 
     return model
 
-model = load_model()
+def load_top500_predictor():
+    with open('xgb_top500.pkl', 'rb') as file:
+        model = pickle.load(file)
+
+    return model
+
+status_predictor = load_status_predictor()
+top500_predictor = load_top500_predictor()
 
 @app.route('/')
 def home():
-    return 'HOME'
+    return 'I <3 ADVAY VYAS!'
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -22,9 +29,10 @@ def predict():
 
     df = pd.DataFrame(data, index=[0])
 
-    predictions = model.predict(df)
+    status_predictions = status_predictor.predict(data)
+    top500_predictions = top500_predictor.predict(data)
 
-    return jsonify({'predictions': predictions.tolist()})
+    return jsonify({'status_predictions': status_predictions.tolist(), 'top500_prediction': top500_predictions.tolist()})
 
 if __name__ == '__main__':
     app.run(debug=True)
